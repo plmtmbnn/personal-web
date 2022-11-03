696f077fb78bae612ee9aaf16845a09dfd5b7666
polmatambunan <polma.tambunan@fit.id>
2022-11-03 17:31:34 +0700

    fix

--- pages/index.js
+++ pages/index.js
@@ -6,7 +6,7 @@ export default function Home() {
   return (
     <div className="container">
       <Head>
-        <title>Next.js Starter!</title>
+        <title>Polma Tambunan - Personal Website</title>
         <link rel="icon" href="/favicon.ico" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
   <meta name="theme-color" content="#000000" />
