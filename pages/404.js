d58d87257222bfc4f7f48aaad3863baee7a56cb4
polmatambunan <polma.tambunan@fit.id>
2023-04-28 18:52:52 +0700

    adjust style

--- pages/404.js
+++ pages/404.js
@@ -1,13 +1,28 @@
 import React from "react";
 import Link from "next/link";
-import { Container } from "reactstrap";
+import { Col, Container, Row } from "reactstrap";
+import NavBar from "../components/NavBar";
 
 export default function Custom404() {
   return (
     <Container>
-      <h1>404 - Page Not Found</h1>
-      <hr />
-      <Link href="/">Go back home</Link>
+      <NavBar />
+
+      <Row style={{ height: "75vh" }} className="align-items-center">
+        <Col sm="12" className="d-flex align-self-end justify-content-center">
+          <h1>404</h1>
+        </Col>
+        <Col sm="12" className="d-flex align-self-start justify-content-center">
+          <h2 class="font-weight-normal lead" id="desc">
+            The page you requested was not found.
+          </h2>
+        </Col>
+        <Col sm="12" className="d-flex align-self-start justify-content-center">
+          <Link href="/" className="justify-self-center">
+            Go back home
+          </Link>
+        </Col>
+      </Row>
     </Container>
   );
 }
