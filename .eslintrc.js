58a727428579725d86750e1080ab5dc8582dd69c
polmatambunan <plmtmbnn@gmail.com>
2025-01-25 13:06:49 +0700

    chore: library eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,14 +1,34 @@
 module.exports = {
-    extends: ['eslint:recommended', 'plugin:react/recommended', 'plugin:react-hooks/recommended', 'plugin:jsx-a11y/recommended'],
-    plugins: ['react', 'react-hooks', 'jsx-a11y', 'prettier'],
-    parserOptions: {
-      ecmaVersion: 2021,
-      sourceType: 'module',
-      ecmaFeatures: {
-        jsx: true,
-      },
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:react-hooks/recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:jsx-a11y/recommended',
+    'plugin:prettier/recommended', // Ensure Prettier is integrated with ESLint
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
     },
-    rules: {
-      'prettier/prettier': 'error',
+    ecmaVersion: 2021,
+    sourceType: 'module',
+  },
+  plugins: ['react', '@typescript-eslint', 'jsx-a11y', 'prettier'],
+  settings: {
+    react: {
+      version: 'detect',
     },
-  };
+  },
+  rules: {
+    'react/react-in-jsx-scope': 'off', // Next.js/Vite doesn't require React import
+    '@typescript-eslint/no-unused-vars': ['error'],
+    'react/prop-types': 'off',
+    'prettier/prettier': 'error', // Ensures Prettier rules are enforced
+  },
+};
